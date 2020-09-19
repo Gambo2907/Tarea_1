39641c1e8ff8fe87343f71d20368a63ad6887d57
Gambo2907 <67343582+Gambo2907@users.noreply.github.com>
2020-09-19 00:35:24 -0600

    Create ChatApp.java
    Primera clase donde se busca hacer la interfaz gráfica

---
+++ src/ChatApp.java
@@ -0,0 +1,18 @@
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class ChatApp extends Application {
+
+	@Override
+	public void start(Stage primaryStage) throws Exception {
+		primaryStage.show();
+		
+
+	}
+
+	public static void main(String[] args) {
+		launch(args);
+		
+	}
+
+}
